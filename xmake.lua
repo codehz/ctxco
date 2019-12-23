40135abe21e8d4ebb9bcace6b32b422f8fbc5963
CodeHz <CodeHz@outlook.com>
2019-12-23 23:36:58 +0800

    fix option

--- xmake.lua
+++ xmake.lua
@@ -25,16 +25,35 @@ if is_mode("release") then
     add_vectorexts("sse2", "sse3", "ssse3", "mmx")
 end
 
+option("shared")
+    set_default(false)
+    set_showmenu(true)
+    set_category("option")
+    set_description("Enable or disable shared mode")
+option_end()
+
+option("demo")
+    set_default(false)
+    set_showmenu(true)
+    set_category("option")
+    set_description("Enable or disable demo")
+option_end()
+
 target("ctxco")
-    add_cxflags("-fPIC")
-    add_ldflags("-fPIC")
-    set_kind("shared")
+    if has_config("shared") then
+        add_cxflags("-fPIC")
+        add_ldflags("-fPIC")
+        set_kind("shared")
+    else
+        set_kind("static")
+    end
+    set_default(true)
     add_files("src/ctxco/*.c", "src/ctxco/ctx.S")
     add_headerfiles("src/ctxco/ctx.h", "src/ctxco/ctxco.h")
 target_end()
 
 target("demo_simple")
-    set_default(false)
+    set_default(has_config("demo"))
     set_kind("binary")
     add_deps("ctxco")
     add_packages("ctxco")
