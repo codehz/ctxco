78f69f02419a51b49972d8f15cee6a1fe944b08a
CodeHz <CodeHz@outlook.com>
2019-12-24 14:42:02 +0800

    fix xmake

---
+++ src/ctxco/ctx.d
@@ -0,0 +1,1 @@
+ctx.o: ctx.S /usr/include/stdc-predef.h arch/x64.S
--- xmake.lua
+++ xmake.lua
@@ -4,33 +4,7 @@ set_xmakever("2.2.8")
 set_warnings("all", "error")
 set_languages("gnu15")
 
-add_rules("mode.debug", "mode.release")
-
-if is_mode("debug") then
-    set_symbols("debug")
-    set_optimize("none")
-    if is_mode("check") and is_arch("i386", "x86_64") then
-        add_cxflags("-fsanitize=address", "-ftrapv")
-        add_mxflags("-fsanitize=address", "-ftrapv")
-        add_ldflags("-fsanitize=address")
-    end
-end
-
-if is_mode("release") then
-    set_symbols("hide")
-    set_strip("all")
-    add_cxflags("-fomit-frame-pointer")
-    add_mxflags("-fomit-frame-pointer")
-    set_optimize("fastest")
-    add_vectorexts("sse2", "sse3", "ssse3", "mmx")
-end
-
-option("shared")
-    set_default(false)
-    set_showmenu(true)
-    set_category("option")
-    set_description("Enable or disable shared mode")
-option_end()
+add_rules("mode.debug", "mode.release", "mode.check")
 
 option("demo")
     set_default(false)
@@ -40,16 +14,14 @@ option("demo")
 option_end()
 
 target("ctxco")
-    if has_config("shared") then
+    if is_kind("shared") then
         add_cxflags("-fPIC")
         add_ldflags("-fPIC")
-        set_kind("shared")
-    else
-        set_kind("static")
     end
+    set_kind("$(kind)")
     set_default(true)
     add_files("src/ctxco/*.c", "src/ctxco/ctx.S")
-    add_headerfiles("src/ctxco/ctx.h", "src/ctxco/ctxco.h")
+    add_headerfiles("src/(ctxco/*.h)")
 target_end()
 
 target("demo_simple")
