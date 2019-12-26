f44d11c3c2456434912e631fe131bba4b814e68d
CodeHz <CodeHz@outlook.com>
2019-12-26 15:16:18 +0800

    fix code

--- src/demo/uring.c
+++ src/demo/uring.c
@@ -20,16 +20,17 @@ void io_uring_poller(void *priv, ctxco_request_ref_t co) {
 
     if (co == NULL) {
         if (ref->count == 0) return;
-        struct io_uring_cqe *cqes[CQE_BATCH] = {};
-        unsigned len                         = io_uring_peek_batch_cqe(&ref->ring, cqes, CQE_BATCH);
-        for (unsigned i = 0; i < len; i++) {
-            io_uring_cqe_seen(&ref->ring, cqes[i]);
-            ctxco_impl_t ctx = (ctxco_impl_t) io_uring_cqe_get_data(cqes[i]);
-            ctxco_resume(ctx, (void *) (ssize_t) cqes[i]->res);
+        unsigned i;
+        struct io_uring_cqe *cqe;
+        io_uring_for_each_cqe(&ref->ring, i, cqe) {
+            io_uring_cqe_seen(&ref->ring, cqe);
+            ctxco_impl_t ctx = (ctxco_impl_t) io_uring_cqe_get_data(cqe);
+            ctxco_resume(ctx, (void *) (ssize_t) cqe->res);
             ref->count--;
         }
     } else if (co == CTXCO_BLOCK) {
         if (ref->count == 0) return;
+        io_uring_submit(&ref->ring);
         struct io_uring_cqe *cqe = NULL;
         int ret                  = io_uring_wait_cqe(&ref->ring, &cqe);
         if (ret < 0) { return; }
@@ -41,153 +42,156 @@ void io_uring_poller(void *priv, ctxco_request_ref_t co) {
     } else {
         struct io_uring_sqe *sqe = io_uring_get_sqe(&ref->ring);
         if (!sqe) {
-            ctxco_resume(co->ctx, NULL);
-        } else {
-            switch (co->op) {
-            case IORING_OP_READV: {
-                int fd                     = va_arg(co->arg, int);
-                const struct iovec *iovecs = va_arg(co->arg, const struct iovec *);
-                unsigned nr_vecs           = va_arg(co->arg, unsigned);
-                off_t offset               = va_arg(co->arg, off_t);
-                io_uring_prep_readv(sqe, fd, iovecs, nr_vecs, offset);
-            } break;
-            case IORING_OP_READ_FIXED: {
-                int fd              = va_arg(co->arg, int);
-                void *buf           = va_arg(co->arg, void *);
-                unsigned int nbytes = va_arg(co->arg, unsigned);
-                off_t offset        = va_arg(co->arg, off_t);
-                int buf_index       = va_arg(co->arg, int);
-                io_uring_prep_read_fixed(sqe, fd, buf, nbytes, offset, buf_index);
-            } break;
-            case IORING_OP_WRITEV: {
-                int fd                     = va_arg(co->arg, int);
-                const struct iovec *iovecs = va_arg(co->arg, const struct iovec *);
-                unsigned nr_vecs           = va_arg(co->arg, unsigned);
-                off_t offset               = va_arg(co->arg, off_t);
-                io_uring_prep_writev(sqe, fd, iovecs, nr_vecs, offset);
-            } break;
-            case IORING_OP_WRITE_FIXED: {
-                int fd              = va_arg(co->arg, int);
-                const void *buf     = va_arg(co->arg, void const *);
-                unsigned int nbytes = va_arg(co->arg, unsigned);
-                off_t offset        = va_arg(co->arg, off_t);
-                int buf_index       = va_arg(co->arg, int);
-                io_uring_prep_write_fixed(sqe, fd, buf, nbytes, offset, buf_index);
-            } break;
-            case IORING_OP_RECVMSG: {
-                int fd             = va_arg(co->arg, int);
-                struct msghdr *msg = va_arg(co->arg, struct msghdr *);
-                unsigned int flags = va_arg(co->arg, unsigned);
-                io_uring_prep_recvmsg(sqe, fd, msg, flags);
-            } break;
-            case IORING_OP_SENDMSG: {
-                int fd                   = va_arg(co->arg, int);
-                const struct msghdr *msg = va_arg(co->arg, struct msghdr const *);
-                unsigned int flags       = va_arg(co->arg, unsigned);
-                io_uring_prep_sendmsg(sqe, fd, msg, flags);
-            } break;
-            case IORING_OP_POLL_ADD: {
-                int fd          = va_arg(co->arg, int);
-                short poll_mask = va_arg(co->arg, int);
-                io_uring_prep_poll_add(sqe, fd, poll_mask);
-            } break;
-            case IORING_OP_POLL_REMOVE: {
-                void *user_data = va_arg(co->arg, void *);
-                io_uring_prep_poll_remove(sqe, user_data);
-            } break;
-            case IORING_OP_FSYNC: {
-                int fd                   = va_arg(co->arg, int);
-                unsigned int fsync_flags = va_arg(co->arg, unsigned);
-                io_uring_prep_fsync(sqe, fd, fsync_flags);
-            } break;
-            case IORING_OP_NOP: {
-                io_uring_prep_nop(sqe);
-            } break;
-            case IORING_OP_TIMEOUT: {
-                struct __kernel_timespec *ts = va_arg(co->arg, struct __kernel_timespec *);
-                unsigned count               = va_arg(co->arg, unsigned);
-                unsigned flags               = va_arg(co->arg, unsigned);
-                io_uring_prep_timeout(sqe, ts, count, flags);
-            } break;
-            case IORING_OP_TIMEOUT_REMOVE: {
-                __u64 user_data    = va_arg(co->arg, uint64_t);
-                unsigned int flags = va_arg(co->arg, unsigned);
-                io_uring_prep_timeout_remove(sqe, user_data, flags);
-            } break;
-            case IORING_OP_ACCEPT: {
-                int fd                = va_arg(co->arg, int);
-                struct sockaddr *addr = va_arg(co->arg, struct sockaddr *);
-                socklen_t *addrlen    = va_arg(co->arg, socklen_t *);
-                int flags             = va_arg(co->arg, int);
-                io_uring_prep_accept(sqe, fd, addr, addrlen, flags);
-            } break;
-            case IORING_OP_ASYNC_CANCEL: {
-                void *user_data = va_arg(co->arg, void *);
-                int flags       = va_arg(co->arg, unsigned);
-                io_uring_prep_cancel(sqe, user_data, flags);
-            } break;
-            case IORING_OP_LINK_TIMEOUT: {
-                struct __kernel_timespec *ts = va_arg(co->arg, struct __kernel_timespec *);
-                unsigned flags               = va_arg(co->arg, unsigned);
-                io_uring_prep_link_timeout(sqe, ts, flags);
-            } break;
-            case IORING_OP_CONNECT: {
-                int fd                = va_arg(co->arg, int);
-                struct sockaddr *addr = va_arg(co->arg, struct sockaddr *);
-                socklen_t addrlen     = va_arg(co->arg, socklen_t);
-                io_uring_prep_connect(sqe, fd, addr, addrlen);
-            } break;
-            case IORING_OP_FILES_UPDATE: {
-                int *fds            = va_arg(co->arg, int *);
-                unsigned int nr_fds = va_arg(co->arg, unsigned);
-                io_uring_prep_files_update(sqe, fds, nr_fds);
-            } break;
-            case IORING_OP_FALLOCATE: {
-                int fd       = va_arg(co->arg, int);
-                int mode     = va_arg(co->arg, int);
-                off_t offset = va_arg(co->arg, off_t);
-                off_t len    = va_arg(co->arg, off_t);
-                io_uring_prep_fallocate(sqe, fd, mode, offset, len);
-            } break;
-            case IORING_OP_OPENAT: {
-                int dfd          = va_arg(co->arg, int);
-                const char *path = va_arg(co->arg, char const *);
-                int flags        = va_arg(co->arg, int);
-                mode_t mode      = va_arg(co->arg, mode_t);
-                io_uring_prep_openat(sqe, dfd, path, flags, mode);
-            } break;
-            case IORING_OP_CLOSE: {
-                int fd = va_arg(co->arg, int);
-                io_uring_prep_close(sqe, fd);
-            } break;
-            case IORING_OP_READ: {
-                int fd              = va_arg(co->arg, int);
-                void *buf           = va_arg(co->arg, void *);
-                unsigned int nbytes = va_arg(co->arg, unsigned);
-                off_t offset        = va_arg(co->arg, off_t);
-                io_uring_prep_read(sqe, fd, buf, nbytes, offset);
-            } break;
-            case IORING_OP_WRITE: {
-                int fd              = va_arg(co->arg, int);
-                void *buf           = va_arg(co->arg, void *);
-                unsigned int nbytes = va_arg(co->arg, unsigned);
-                off_t offset        = va_arg(co->arg, off_t);
-                io_uring_prep_write(sqe, fd, buf, nbytes, offset);
-            } break;
-            case IORING_OP_STATX: {
-                int dfd                = va_arg(co->arg, int);
-                const char *path       = va_arg(co->arg, char const *);
-                int flags              = va_arg(co->arg, int);
-                unsigned int mask      = va_arg(co->arg, unsigned);
-                struct statx *statxbuf = va_arg(co->arg, struct statx *);
-                io_uring_prep_statx(sqe, dfd, path, flags, mask, statxbuf);
-            } break;
-            default: io_uring_prep_nop(sqe);
-            }
-            io_uring_sqe_set_data(sqe, co->ctx);
             io_uring_submit(&ref->ring);
-            ref->count++;
+            sqe = io_uring_get_sqe(&ref->ring);
+            if (!sqe) {
+                ctxco_resume(co->ctx, (void *) (ssize_t) -EAGAIN);
+                return;
+            }
         }
+        switch (co->op) {
+        case IORING_OP_READV: {
+            int fd                     = va_arg(co->arg, int);
+            const struct iovec *iovecs = va_arg(co->arg, const struct iovec *);
+            unsigned nr_vecs           = va_arg(co->arg, unsigned);
+            off_t offset               = va_arg(co->arg, off_t);
+            io_uring_prep_readv(sqe, fd, iovecs, nr_vecs, offset);
+        } break;
+        case IORING_OP_READ_FIXED: {
+            int fd              = va_arg(co->arg, int);
+            void *buf           = va_arg(co->arg, void *);
+            unsigned int nbytes = va_arg(co->arg, unsigned);
+            off_t offset        = va_arg(co->arg, off_t);
+            int buf_index       = va_arg(co->arg, int);
+            io_uring_prep_read_fixed(sqe, fd, buf, nbytes, offset, buf_index);
+        } break;
+        case IORING_OP_WRITEV: {
+            int fd                     = va_arg(co->arg, int);
+            const struct iovec *iovecs = va_arg(co->arg, const struct iovec *);
+            unsigned nr_vecs           = va_arg(co->arg, unsigned);
+            off_t offset               = va_arg(co->arg, off_t);
+            io_uring_prep_writev(sqe, fd, iovecs, nr_vecs, offset);
+        } break;
+        case IORING_OP_WRITE_FIXED: {
+            int fd              = va_arg(co->arg, int);
+            const void *buf     = va_arg(co->arg, void const *);
+            unsigned int nbytes = va_arg(co->arg, unsigned);
+            off_t offset        = va_arg(co->arg, off_t);
+            int buf_index       = va_arg(co->arg, int);
+            io_uring_prep_write_fixed(sqe, fd, buf, nbytes, offset, buf_index);
+        } break;
+        case IORING_OP_RECVMSG: {
+            int fd             = va_arg(co->arg, int);
+            struct msghdr *msg = va_arg(co->arg, struct msghdr *);
+            unsigned int flags = va_arg(co->arg, unsigned);
+            io_uring_prep_recvmsg(sqe, fd, msg, flags);
+        } break;
+        case IORING_OP_SENDMSG: {
+            int fd                   = va_arg(co->arg, int);
+            const struct msghdr *msg = va_arg(co->arg, struct msghdr const *);
+            unsigned int flags       = va_arg(co->arg, unsigned);
+            io_uring_prep_sendmsg(sqe, fd, msg, flags);
+        } break;
+        case IORING_OP_POLL_ADD: {
+            int fd          = va_arg(co->arg, int);
+            short poll_mask = va_arg(co->arg, int);
+            io_uring_prep_poll_add(sqe, fd, poll_mask);
+        } break;
+        case IORING_OP_POLL_REMOVE: {
+            void *user_data = va_arg(co->arg, void *);
+            io_uring_prep_poll_remove(sqe, user_data);
+        } break;
+        case IORING_OP_FSYNC: {
+            int fd                   = va_arg(co->arg, int);
+            unsigned int fsync_flags = va_arg(co->arg, unsigned);
+            io_uring_prep_fsync(sqe, fd, fsync_flags);
+        } break;
+        case IORING_OP_NOP: {
+            io_uring_prep_nop(sqe);
+        } break;
+        case IORING_OP_TIMEOUT: {
+            struct __kernel_timespec *ts = va_arg(co->arg, struct __kernel_timespec *);
+            unsigned count               = va_arg(co->arg, unsigned);
+            unsigned flags               = va_arg(co->arg, unsigned);
+            io_uring_prep_timeout(sqe, ts, count, flags);
+        } break;
+        case IORING_OP_TIMEOUT_REMOVE: {
+            __u64 user_data    = va_arg(co->arg, uint64_t);
+            unsigned int flags = va_arg(co->arg, unsigned);
+            io_uring_prep_timeout_remove(sqe, user_data, flags);
+        } break;
+        case IORING_OP_ACCEPT: {
+            int fd                = va_arg(co->arg, int);
+            struct sockaddr *addr = va_arg(co->arg, struct sockaddr *);
+            socklen_t *addrlen    = va_arg(co->arg, socklen_t *);
+            int flags             = va_arg(co->arg, int);
+            io_uring_prep_accept(sqe, fd, addr, addrlen, flags);
+        } break;
+        case IORING_OP_ASYNC_CANCEL: {
+            void *user_data = va_arg(co->arg, void *);
+            int flags       = va_arg(co->arg, unsigned);
+            io_uring_prep_cancel(sqe, user_data, flags);
+        } break;
+        case IORING_OP_LINK_TIMEOUT: {
+            struct __kernel_timespec *ts = va_arg(co->arg, struct __kernel_timespec *);
+            unsigned flags               = va_arg(co->arg, unsigned);
+            io_uring_prep_link_timeout(sqe, ts, flags);
+        } break;
+        case IORING_OP_CONNECT: {
+            int fd                = va_arg(co->arg, int);
+            struct sockaddr *addr = va_arg(co->arg, struct sockaddr *);
+            socklen_t addrlen     = va_arg(co->arg, socklen_t);
+            io_uring_prep_connect(sqe, fd, addr, addrlen);
+        } break;
+        case IORING_OP_FILES_UPDATE: {
+            int *fds            = va_arg(co->arg, int *);
+            unsigned int nr_fds = va_arg(co->arg, unsigned);
+            io_uring_prep_files_update(sqe, fds, nr_fds);
+        } break;
+        case IORING_OP_FALLOCATE: {
+            int fd       = va_arg(co->arg, int);
+            int mode     = va_arg(co->arg, int);
+            off_t offset = va_arg(co->arg, off_t);
+            off_t len    = va_arg(co->arg, off_t);
+            io_uring_prep_fallocate(sqe, fd, mode, offset, len);
+        } break;
+        case IORING_OP_OPENAT: {
+            int dfd          = va_arg(co->arg, int);
+            const char *path = va_arg(co->arg, char const *);
+            int flags        = va_arg(co->arg, int);
+            mode_t mode      = va_arg(co->arg, mode_t);
+            io_uring_prep_openat(sqe, dfd, path, flags, mode);
+        } break;
+        case IORING_OP_CLOSE: {
+            int fd = va_arg(co->arg, int);
+            io_uring_prep_close(sqe, fd);
+        } break;
+        case IORING_OP_READ: {
+            int fd              = va_arg(co->arg, int);
+            void *buf           = va_arg(co->arg, void *);
+            unsigned int nbytes = va_arg(co->arg, unsigned);
+            off_t offset        = va_arg(co->arg, off_t);
+            io_uring_prep_read(sqe, fd, buf, nbytes, offset);
+        } break;
+        case IORING_OP_WRITE: {
+            int fd              = va_arg(co->arg, int);
+            void *buf           = va_arg(co->arg, void *);
+            unsigned int nbytes = va_arg(co->arg, unsigned);
+            off_t offset        = va_arg(co->arg, off_t);
+            io_uring_prep_write(sqe, fd, buf, nbytes, offset);
+        } break;
+        case IORING_OP_STATX: {
+            int dfd                = va_arg(co->arg, int);
+            const char *path       = va_arg(co->arg, char const *);
+            int flags              = va_arg(co->arg, int);
+            unsigned int mask      = va_arg(co->arg, unsigned);
+            struct statx *statxbuf = va_arg(co->arg, struct statx *);
+            io_uring_prep_statx(sqe, dfd, path, flags, mask, statxbuf);
+        } break;
+        default: io_uring_prep_nop(sqe);
+        }
+        io_uring_sqe_set_data(sqe, co->ctx);
+        ref->count++;
     }
 }
 
@@ -203,6 +207,8 @@ void client_co(void *priv);
 void server_co(void *priv) {
     int server = socket(AF_INET6, SOCK_STREAM | SOCK_CLOEXEC, IPPROTO_TCP);
     if (server < 0) fatal("Failed to create socket: %s\n", strerror(errno));
+    int option = 1;
+    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &option, sizeof option);
 
     struct sockaddr_in6 addr = {};
     addr.sin6_family         = AF_INET6;
@@ -241,7 +247,7 @@ void client_co(void *priv) {
         ssize_t ret    = (ssize_t) ctxco_invoke(IORING_OP_RECVMSG, client, &msg, 0);
         if (ret <= 0) break;
         iov.iov_len = ret;
-        ret = (ssize_t) ctxco_invoke(IORING_OP_SENDMSG, client, &msg, 0);
+        ret         = (ssize_t) ctxco_invoke(IORING_OP_SENDMSG, client, &msg, 0);
         if (ret <= 0) break;
     }
     close(client);
@@ -249,7 +255,8 @@ void client_co(void *priv) {
 
 int main() {
     poller_data_t poller_data;
-    io_uring_queue_init(32, &poller_data.ring, 0);
+    int ret = io_uring_queue_init(32, &poller_data.ring, 0);
+    if (ret < 0) fatal("Failed to init io_uring: %s\n", strerror(errno));
     ctxco_init(io_uring_poller, &poller_data);
 
     ctxco_start(server_co, NULL, 128 * 1024);
